c057f25e7ee5b087c0a95fa0373b5a5fd8226dd1
TraceLD <lukaszdabrowski881@gmail.com>
2021-06-05 03:25:34 +0200

    add tailwind

--- src/TraceLd.GitHubLangStats.Frontend/rollup.config.js
+++ src/TraceLd.GitHubLangStats.Frontend/rollup.config.js
@@ -40,7 +40,10 @@ export default {
 	},
 	plugins: [
 		svelte({
-			preprocess: sveltePreprocess({ sourceMap: !production }),
+			preprocess: sveltePreprocess({ 
+				sourceMap: !production,
+				postcss: true,
+			}),
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
