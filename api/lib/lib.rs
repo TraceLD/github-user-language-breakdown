38d4a113094f3e5a6365f6fe34280bb0b58e1371
Lukasz Dabrowski <ldabrowski@airspan.com>
2024-09-21 21:26:56 +0100

    Clean up API code

--- api/lib/lib.rs
+++ api/lib/lib.rs
@@ -1,15 +1,4 @@
-use octocrab::Octocrab;
-
-use std::sync::LazyLock;
-
 pub mod api_utils;
-pub mod github_auth;
+pub mod config;
 pub mod github_fetch;
 pub mod langs_calculator;
-
-pub static CRAB: LazyLock<Octocrab> = LazyLock::new(|| {
-    Octocrab::builder()
-        .personal_token(crate::github_auth::get_gh_token().unwrap())
-        .build()
-        .unwrap()
-});
